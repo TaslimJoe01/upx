443f4d84ceca748f39c0c6d9304ceacc719d7e75
TaslimJoe01 <162861113+TaslimJoe01@users.noreply.github.com>
2026-02-08 12:45:24 +0700

    upx-mod

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
 #define UPX_VERSION_HEX      0x010000 /* 01.00.00 */
-#define UPX_VERSION_STRING   "1.00"
+#define UPX_VERSION_STRING   "1.0.0"
 #define UPX_VERSION_STRING4  "1.00"
-#define UPX_VERSION_DATE     "Jan 1th 2019"
+#define UPX_VERSION_DATE     "Jan 1st 2019"
 #define UPX_VERSION_DATE_ISO "2019-01-01"
 #define UPX_VERSION_YEAR     "2019"
