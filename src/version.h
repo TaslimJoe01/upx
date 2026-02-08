2ff25603980a7e6208697cc7e1d47346b4c289dc
TaslimJoe01 <162861113+TaslimJoe01@users.noreply.github.com>
2026-02-08 11:57:41 +0700

    upx-mod

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #pragma once
-#define UPX_VERSION_HEX      0x050101 /* 05.01.01 */
-#define UPX_VERSION_STRING   "5.1.1"
-#define UPX_VERSION_STRING4  "5.11"
-#define UPX_VERSION_DATE     "Jan 8th 2026"
-#define UPX_VERSION_DATE_ISO "2026-01-08"
-#define UPX_VERSION_YEAR     "2026"
+#define UPX_VERSION_HEX      0x010000 /* 01.00.00 */
+#define UPX_VERSION_STRING   "1.00"
+#define UPX_VERSION_STRING4  "1.00"
+#define UPX_VERSION_DATE     "Jan 1th 2019"
+#define UPX_VERSION_DATE_ISO "2019-01-01"
+#define UPX_VERSION_YEAR     "2019"
